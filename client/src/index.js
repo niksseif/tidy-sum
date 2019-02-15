32bf812b8dd698cdec917bf01b02136d7462b2f0
NikSS <nik.safe@gmail.com>
2019-02-15 14:47:21 -0800

    comment out the redux imports

--- client/src/index.js
+++ client/src/index.js
@@ -9,11 +9,12 @@ import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
 
-const store = createStore(reducers, {},applyMiddleware(reduxThunk) )
+// const store = createStore(reducers, {},applyMiddleware(reduxThunk) )
 ReactDOM.render(
-    <Provider store ={store}>
+    // <Provider store ={store}>
         <App />
-    </Provider>,
+    // </Provider>
+    ,
 document.getElementById('root')
 );
 
