6c068a42be857988f4191affa5933b23507a39d9
NikSS <nik.safe@gmail.com>
2019-02-26 15:20:32 -0800

    changed the URL to proxy

--- client/src/api/deleteIncome.js
+++ client/src/api/deleteIncome.js
@@ -1,7 +1,7 @@
 
 const URL = 'http://localhost:5000';
 const deleteIncome = (id, userId) => {
-  const res = fetch(`${URL}/users/${userId}/income/${id}`, {
+  const res = fetch(`/users/${userId}/income/${id}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
