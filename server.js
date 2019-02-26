a3e35dcf0d157bcc3f6f4bc84103b9ebc2de6ad0
NikSS <nik.safe@gmail.com>
2019-02-26 13:15:56 -0800

    make it better

--- server.js
+++ server.js
@@ -1,53 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
-const morgan = require('morgan')
-const cors = require('cors')
+const morgan = require('morgan');
+const cors = require('cors');
+
 const port = process.env.PORT || 5000;
 
 
-//============================LOCAL HOST LINKS
-//GETTING ALL USERS
+//= ===========================LOCAL HOST LINKS
+// GETTING ALL USERS
 // http://localhost:5000/users
 
-//GETTING THE INCOME OF SPECIFIC USER
-//http://localhost:5000/users/user:id/income
+// GETTING THE INCOME OF SPECIFIC USER
+// http://localhost:5000/users/user:id/income
 
-//GETTING ALL INCOMES
+// GETTING ALL INCOMES
 // http://localhost:5000/income
 
-//GETTING ALL EXPENSES
+// GETTING ALL EXPENSES
 // http://localhost:5000/expense
-//=================================================
+//= ================================================
 
 
-
-if (process.env.NODE_ENV !== 'test') app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(cors())
-
-
-const userRoutes = require('./src/routes/users')
-const incomeRoute = require('./src/routes/income')
-const expenseRoute = require('./src/routes/expense')
+if (process.env.NODE_ENV !== 'test') app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
 
 
-//user route
-app.use('/users',userRoutes)
-
-//income route
-app.use('/income',incomeRoute)
-
-//expense route
-app.use('/expense', expenseRoute)
+const userRoutes = require('./src/routes/users');
+const incomeRoute = require('./src/routes/income');
+const expenseRoute = require('./src/routes/expense');
 
 
+// user route
+app.use('/users', userRoutes);
+
+// income route
+app.use('/income', incomeRoute);
+
+// expense route
+app.use('/expense', expenseRoute);
 
 
-
-app.all('*', (req, res, next) => res.sendStatus(404))
+app.all('*', (req, res, next) => res.sendStatus(404));
 
 app.use((err, req, res, next) => {
-    res.status(err.status).json(err)
-})
+  res.status(err.status).json(err);
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
