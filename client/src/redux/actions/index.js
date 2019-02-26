d57f8d8533472e6ef4e4680ff029edaff8591892
NikSS <nik.safe@gmail.com>
2019-02-26 13:11:53 -0800

    fixed package json on clinet side and serverside, everything works locally

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,183 +1,162 @@
-import { ADD_USER_INCOME, ADD_USER_INCOME_FAILED, FETCH_USER_EXPENSE, ADD_USER_EXPENSE, ADD_USER_EXPENSE_FAILED, EDIT_USER_EXPENSE, DELETE_USER_EXPENSE } from './types'
-import { FETCH_USER_INCOME, FETCH_USER_DATA_SUCCESS, FETCH_USER_DATA_FAILED, DELETE_USER_INCOME, EDIT_USER_INCOME} from './types'
+import { ADD_USER_INCOME, ADD_USER_INCOME_FAILED, FETCH_USER_EXPENSE, ADD_USER_EXPENSE, ADD_USER_EXPENSE_FAILED, EDIT_USER_EXPENSE, DELETE_USER_EXPENSE } from './types';
+import { FETCH_USER_INCOME, FETCH_USER_DATA_SUCCESS, FETCH_USER_DATA_FAILED, DELETE_USER_INCOME, EDIT_USER_INCOME } from './types';
 
 
+// const URL = 'http://localhost:5000'
+// http://localhost:5000/users/${id}/income
 
-
-const URL = 'http://localhost:5000'
-    //http://localhost:5000/users/${id}/income
-
-//Add post request to the user income module
+// Add post request to the user income module
 
 
 export const fetchUserData = () => async (dispatch) => {
-   try{ 
-       let res = await fetch(`${URL}/users`);
-        let usersData = await res.json()
-        dispatch({ 
-            type: FETCH_USER_DATA_SUCCESS, 
-            payload: usersData
-        });  
-        console.log(res,"<<<Res from user fetch")
-   } catch (err) {
-       dispatch({
-           type: FETCH_USER_DATA_FAILED,
-           payload: err,
-       })
-   }
-}
-
-
-export const fetchUserIncome  = (id) => async (dispatch) =>{
-    try {
-        let res = await fetch(`${URL}/users/${id}/income`)
-        let userIncome = await res.json();
-
-        dispatch({ type: FETCH_USER_INCOME, payload: userIncome})
-    } catch (err) {
-        throw err;
-    }
-}
-
-
-
-//ADD USER INCOME
-export const handleAdd =  (data) => async (dispatch) =>  {
-
-    try {
-        const res = await fetch(`http://localhost:5000/users/${data.users_id}/income`, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data)
-    })
-    let result = await res.json()
+  try {
+    const res = await fetch('/users');
+    const usersData = await res.json();
     dispatch({
-        type: ADD_USER_INCOME, 
-        payload : result
-    })
-    } catch (err) {
-        dispatch({
-        type: ADD_USER_INCOME_FAILED,
-        payload:err,
-        })
-    }
-}
+      type: FETCH_USER_DATA_SUCCESS,
+      payload: usersData,
+    });
+  } catch (err) {
+    dispatch({
+      type: FETCH_USER_DATA_FAILED,
+      payload: err,
+    });
+  }
+};
+
+
+export const fetchUserIncome = id => async (dispatch) => {
+  try {
+    const res = await fetch(`/users/${id}/income`);
+    const userIncome = await res.json();
+
+    dispatch({ type: FETCH_USER_INCOME, payload: userIncome });
+  } catch (err) {
+    throw err;
+  }
+};
+
+
+// ADD USER INCOME
+export const handleAdd = data => async (dispatch) => {
+  try {
+    const res = await fetch(`/users/${data.users_id}/income`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    const result = await res.json();
+    dispatch({
+      type: ADD_USER_INCOME,
+      payload: result,
+    });
+  } catch (err) {
+    dispatch({
+      type: ADD_USER_INCOME_FAILED,
+      payload: err,
+    });
+  }
+};
 
 // _______UPDATE USER INCOME______
 
-export const handleUpdate =  (id, data) => async dispatch =>{
-   
-        let res = fetch(`http://localhost:5000/users/${id}/income`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        }).then((data) => {
-            console.log(data, "DATA FROM API")
-            data.json()
-            .then(json => {
-                console.log(json,"<<<<<json object")
-                dispatch({ type: EDIT_USER_INCOME, payload: json })
-            })
-        })
-   
-}
-//______________DELETE USER INCOME
+export const handleUpdate = (id, data) => async (dispatch) => {
+  const res = fetch(`/users/${id}/income`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  }).then((data) => {
+    data.json()
+      .then((json) => {
+        dispatch({ type: EDIT_USER_INCOME, payload: json });
+      });
+  });
+};
+// ______________DELETE USER INCOME
 export const handleDeleteReq = (id, userId) => async (dispatch) => {
+  fetch(`/users/${userId}/income/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
 
-    fetch(`http://localhost:5000/users/${userId}/income/${id}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        },
+  }).then((data) => {
+    data.json().then((json) => {
+      dispatch({ type: DELETE_USER_INCOME, payload: json });
+    });
+  });
+};
 
-    }).then((data) => {
-        console.log(data, "<<< returned data from api")
-        data.json().then(json => {
-            dispatch({ type: DELETE_USER_INCOME, payload: json })
-        })
-    })
+// ------------------FETCH EXPENSE
+export const fetchUserExpense = id => async (dispatch) => {
+  try {
+    const res = await fetch(`/users/${id}/expense`);
+    const userExpense = await res.json();
 
-}
+    dispatch({ type: FETCH_USER_EXPENSE, payload: userExpense });
+  } catch (err) {
+    throw err;
+  }
+};
 
-//------------------FETCH EXPENSE
-export const fetchUserExpense = (id) => async (dispatch) => {
-    try {
-        let res = await fetch(`${URL}/users/${id}/expense`)
-        let userExpense = await res.json();
-        
-        dispatch({ type: FETCH_USER_EXPENSE, payload: userExpense })
-    } catch (err) {
-        throw err;
-    }
-}
+// -------CREATE USER EXPENSE
 
-//-------CREATE USER EXPENSE
-
-export const handleAddExpense = (data) => async (dispatch) => {
-
-    try {
-        const res = await fetch(`http://localhost:5000/users/${data.users_id}/expense`, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        })
-        let result = await res.json()
-        dispatch({
-            type: ADD_USER_EXPENSE,
-            payload: result
-        })
-    } catch (err) {
-        dispatch({
-            type: ADD_USER_EXPENSE_FAILED,
-            payload: err,
-        })
-    }
-}
+export const handleAddExpense = data => async (dispatch) => {
+  try {
+    const res = await fetch(`/users/${data.users_id}/expense`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    const result = await res.json();
+    dispatch({
+      type: ADD_USER_EXPENSE,
+      payload: result,
+    });
+  } catch (err) {
+    dispatch({
+      type: ADD_USER_EXPENSE_FAILED,
+      payload: err,
+    });
+  }
+};
 // _______EDIT USER EXPENSE______
 
-export const editUserExpense = (id, data) => async dispatch => {
-    console.log(data,"<<<<data")
-    console.log(id,"<<<id")
-    let res = fetch(`http://localhost:5000/users/${id}/expense`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data)
-    }).then((data) => {
-        console.log(data, "DATA FROM API")
-            data.json()
-      
-        .then(json => {
-                console.log(json, "<<<<<json object")
-                dispatch({ type: EDIT_USER_EXPENSE, payload: json })
-            })
-    })
+export const editUserExpense = (id, data) => async (dispatch) => {
+  const res = fetch(`/users/${id}/expense`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  }).then((data) => {
+    data.json()
 
-}
+      .then((json) => {
+        dispatch({ type: EDIT_USER_EXPENSE, payload: json });
+      });
+  });
+};
 
-//--------------------DELETE USER EXPENSE--------------
+// --------------------DELETE USER EXPENSE--------------
 export const deleteUserExpense = (id, userId) => async (dispatch) => {
+  fetch(`/users/${userId}/expense/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
 
-    fetch(`http://localhost:5000/users/${userId}/expense/${id}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-
-    }).then((data) => {
-        console.log(data, "<<< returned data from api")
-        data.json().then(json => {
-            dispatch({ type: DELETE_USER_EXPENSE, payload: json })
-        })
-    })
-
-}
+  }).then((data) => {
+    data.json().then((json) => {
+      dispatch({ type: DELETE_USER_EXPENSE, payload: json });
+    });
+  });
+};
