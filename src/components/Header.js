601106d0335d1da39055fa69b3099bb19970f770
NikSS <nik.safe@gmail.com>
2019-02-27 12:55:06 -0800

    move asset folder to src folder

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,58 +3,62 @@ import { Link } from 'react-router-dom';
 import { Image, Segment } from 'semantic-ui-react';
 import balance from '../Assets/balance.jpg';
 import expense from '../Assets/moneyFly.jpg';
-import income from '../Assets/dollarsign.jpg';
-
+import income from '../Assets/dollarsign.jpg'
+;
 class Header extends Component {
   render() {
     return (
-      <div >
-            <Segment style={{
- display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignContent: 'center', backgroundColor: '#DBE2DD',
+          <div >
+              <Segment style={{
+ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignContent: 'center', backgroundColor: '#DBE2DD' 
+}}>
+                  <Link
+to="/dashboard/income"
+                  style={{
+ textAlign: 'center', fontSize: '15px', fontWeight: 'bold', marginTop: '2vw', color: 'black' 
 }}
-              >
-                <Link
-                    to="/dashboard/income"
-                    style={{
- textAlign: 'center', fontSize: '15px', fontWeight: 'bold', marginTop: '2vw', color: 'black',
-}}
-                  >
-                    <Image
-                    src={income}
-                    size="tiny"
-                    style={{ border: '3px solid black', width: '80px', height: '80px' }}
-                  />
-                    <br />
-                    <span >INCOME
-                  </span>
-                  </Link>
-                <Link
-                    to="/dashboard/expense"
-                    style={{
- textAlign: 'center', fontSize: '15px', fontWeight: 'bold', marginTop: '2vw', color: 'black',
-}}
-                  >
-                    <Image
-                    src={expense}
-                    size="tiny"
-                    style={{ border: '3px solid black', width: '80px', height: '80px' }}
-                  />
-                    <br />
-                    <span>EXPENSE</span>
-                  </Link>
-                {/* <Link to='/dashboard/balance'
-                        style={{ textAlign: 'center', fontSize: '15px', fontWeight: 'bold', marginTop: '2vw', color: 'black' }}
                 >
-                    <Image
-                    src={balance}
-                    size='tiny'
-                    style={{ border: '3px solid black', width: '80px', height: '80px' }}
+                  <Image
+                      src={income}
+                      size="tiny"
+                      style={{ border: '3px solid black', width: '80px', height: '80px' }}
                     />
-                    <br></br>
-                    <span >BALANCE</span>
-                </Link> */}
-              </Segment>
-          </div>
+                  <br />
+                  <span
+
+                    >INCOME
+                    </span>
+                </Link>
+                  <Link
+to ="/dashboard/expense"
+                  style={{
+ textAlign: 'center', fontSize: '15px', fontWeight: 'bold', marginTop: '2vw', color: 'black'
+ }}
+                >
+                  <Image
+                      src={expense}
+                      size="tiny"
+                      style={{ border: '3px solid black', width: '80px', height: '80px' }}
+                    />
+                  <br />
+                  <span>EXPENSE</span>
+                </Link>
+                  <Link
+to="/dashboard/balance"
+                  style={{
+ textAlign: 'center', fontSize: '15px', fontWeight: 'bold', marginTop: '2vw', color: 'black' 
+}}
+                >
+                  <Image
+                      src={balance}
+                      size="tiny"
+                      style={{ border: '3px solid black', width: '80px', height: '80px' }}
+                    />
+                  <br />
+                  <span >BALANCE</span>
+                </Link>
+                </Segment>
+            </div>
     );
   }
 }
